54e950f35728bbb779adfdcf13a9f1e18c675995
brillieuv <indahbriliant86@gmailcom>
2026-04-01 07:27:33 +0700

    feat: edit result view

--- views/result.php
+++ views/result.php
@@ -1,7 +1,9 @@
-<div class="">
-    <h2 class="">Result</h2>
-    <li><p>Hi, my name is <?= $data['firstName'] ?> <?= $data['lastName'] ?></p></li>
+<div class="w-full mx-auto mt-10 flex flex-col gap-4 items-center">
+    <h2 class="font-semibold">Result</h2>
+    <ul>
+            <li><p>Hi, my name is <?= $data['firstName'] ?> <?= $data['lastName'] ?></p></li>
     <li><p>Phone Number :  <?= $data['phone'] ?></p></li>
     <li><p>Address :  <?= $data['address'] ?></p></li>
-    
+
+    </ul>    
 </div>
